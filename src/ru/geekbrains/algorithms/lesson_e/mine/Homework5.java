da9a5fac170b697765a2ff1e67d566404b78cb57
senyabird <senyabird@yandex.ru>
2021-01-30 12:54:01 +0300

    recPow method implemented.

--- src/ru/geekbrains/algorithms/lesson_e/mine/Homework5.java
+++ src/ru/geekbrains/algorithms/lesson_e/mine/Homework5.java
@@ -1,4 +1,19 @@
 package ru.geekbrains.algorithms.lesson_e.mine;
 
 public class Homework5 {
+    private static int recPow(int degreeBasis, int degreeIndicator) {
+        if ((degreeBasis == 0 && degreeIndicator <= 0)) throw new RuntimeException("the value is not defined");
+        if (degreeIndicator < 0 && degreeBasis != 1)
+            throw new RuntimeException("this method is not intended for calculating degrees with a negative indicator");
+        if (degreeBasis == 1) return 1;
+        if (degreeBasis != 0 && degreeIndicator == 0) return 1;
+        else {
+            degreeBasis *= recPow(degreeBasis, --degreeIndicator);
+        }
+        return degreeBasis;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(recPow(2, 10));
+    }
 }
